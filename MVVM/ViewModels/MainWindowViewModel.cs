62cb7b832c3303e8a385c0c5a0fbaf0977b0de55
Andrey Salnikov <kvanta13@yandex.ru>
2022-03-04 23:38:45 +0300

    Добавьте файлы проекта.

---
+++ MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MVVM.Models;
+
+namespace MVVM.VievModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChenged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChenged();
+            }
+        }
+        private double number2;
+        public double Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnPropertyChenged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Number2 = Round.round(number1);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Number1 != 0 || Number2 != 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
